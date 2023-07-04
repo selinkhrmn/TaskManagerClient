7206567558cbdb8e2ab48711a344ba446f5ff8e6
Eftelya <20200808026@ogr.akdeniz.edu.tr>
2023-07-04 13:20:49 +0300

    Modules
    -added angular material, forms and cdk modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AllFormsModule, MaterialModule, CdkModule } from './modules';
 import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,13 @@ import { RouterModule } from '@angular/router';
     TabsComponent,
   ],
   imports: [
+    AllFormsModule,
+    MaterialModule,
+    CdkModule,
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
 
   providers: [],
