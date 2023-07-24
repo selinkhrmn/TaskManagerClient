0acc621510fd1ea056ae2faf84cab7f3b8bbe4ac
selinkhrmn <khrmnsln159@gmail.com>
2023-07-24 12:29:47 +0300

    Create listTask.ts

---
+++ src/app/interfaces/listTask.ts
@@ -0,0 +1,16 @@
+export interface ListTask {
+    
+    id: number;
+    name: string;
+    columnId: string;
+    //kategori: string;
+    assigneeId: number;
+    reporterId:number;
+    DueDate: Date;
+    Priority: number;
+    UpdateDate:Date;
+  
+    CreateDate:Date;
+    
+    
+  }
