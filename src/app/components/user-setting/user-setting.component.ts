94a5630165aa7f74fb1880e5ba86be32ec19a211
Cihan <officialcihan0248@gmail.com>
2023-07-26 16:00:06 +0300

    Merge branch 'main' of https://github.com/selinkhrmn/TaskManagerClient

--- src/app/components/user-setting/user-setting.component.ts
+++ src/app/components/user-setting/user-setting.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslocoService} from '@ngneat/transloco';
+import { RegisterPageComponent } from '../register-page/register-page.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-setting',
@@ -7,5 +9,13 @@ import { TranslocoService} from '@ngneat/transloco';
   styleUrls: ['./user-setting.component.scss']
 })
 export class UserSettingComponent {
-constructor(public translocoService : TranslocoService){}
+
+  
+
+constructor(public translocoService : TranslocoService,public dialog: MatDialog){}
+
+
+openDialog(): void {
+  const dialogRef = this.dialog.open(RegisterPageComponent,{height: '90%',width: '70%',panelClass: 'dialog'});
+}
 }
