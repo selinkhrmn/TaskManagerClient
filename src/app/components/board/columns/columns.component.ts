058dc35de19c59dd799dfcc6c442fd70922da1d5
Cihan <officialcihan0248@gmail.com>
2023-09-03 19:55:21 +0300

    css

--- src/app/components/board/columns/columns.component.ts
+++ src/app/components/board/columns/columns.component.ts
@@ -33,7 +33,7 @@ import { TranslocoService } from '@ngneat/transloco';
 import { TokenService } from 'src/app/services/token.service';
 import { TransferColumnTaskComponent } from '../../transfer-column-task/transfer-column-task.component';
 import Swal from 'sweetalert2';
-
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-columns',
@@ -43,10 +43,22 @@ import Swal from 'sweetalert2';
 })
 export class ColumnsComponent {
   predefinedColors: string[] = [
-    '#FF5733', '#33FF57', '#5733FF', '#FF336A',
-    '#33B4FF', '#FFD633', '#A633FF', '#33FFD6',
-    '#FF33C1', '#33FFA6', '#E433FF', '#33E2FF',
-    '#B333FF', '#33FFB4', '#FFA833', '#E633FF'
+    '#FF5733',
+    '#33FF57',
+    '#5733FF',
+    '#FF336A',
+    '#33B4FF',
+    '#FFD633',
+    '#A633FF',
+    '#33FFD6',
+    '#FF33C1',
+    '#33FFA6',
+    '#E433FF',
+    '#33E2FF',
+    '#B333FF',
+    '#33FFB4',
+    '#FFA833',
+    '#E633FF',
   ];
   @Output() currentColumnId: number;
   tasks: Task[] = [];
@@ -70,7 +82,8 @@ export class ColumnsComponent {
   assignedPerson: string[];
   columnsLength: any[] = []; // Your column data
   tallestColumnHeight: string; // Store the tallest column's height
-
+  @ViewChild('taskInput') taskInput: ElementRef;
+  @ViewChild('yourMatMenuTrigger') yourMatMenuTrigger: MatMenuTrigger;
 
   constructor(
     private columnService: ColumnService,
@@ -78,82 +91,101 @@ export class ColumnsComponent {
     public dialog: MatDialog,
     private projectService: ProjectService,
     public translocoService: TranslocoService,
-    public tokenService: TokenService) {
-
-  }
+    public tokenService: TokenService
+  ) {}
 
   ngOnInit(): void {
-    debugger
+    debugger;
 
-    
     this.getColumnId(this.currentColumnId);
     if (this.projectService.getProjectLocal() != null) {
       this.currentProject = this.projectService.getProjectLocal();
-      this.columnService.GetProjectColumnsTasks( this.projectService.getProjectLocal().id ).subscribe((response) => {
-        if (response.data != null) {
-          this.columns = response.data;
+      this.columnService
+        .GetProjectColumnsTasks(this.projectService.getProjectLocal().id)
+        .subscribe((response) => {
+          if (response.data != null) {
+            this.columns = response.data;
 
-          this.updateBackgroundColor();
-          console.log(this.columns);
-          const columnHeights = this.columns.map(column => column.tasks.length * 60.8 + 142); // Assuming each task is 40px tall
-          console.log(columnHeights);
-          
-          this.tallestColumnHeight = Math.max(...columnHeights) + 'px';
-          console.log(this.tallestColumnHeight);
-        }
-      });
+            this.updateBackgroundColor();
+            console.log(this.columns);
+            const columnHeights = this.columns.map(
+              (column) => column.tasks.length * 60.8 + 120
+            ); // Assuming each task is 40px tall
+            console.log(columnHeights);
+
+            this.tallestColumnHeight = Math.max(...columnHeights) + 'px';
+            console.log(this.tallestColumnHeight);
+          }
+        });
     }
-    
-
   }
-  stopPropagation(event: { stopPropagation: () => void; }) {
+  stopPropagation(event: { stopPropagation: () => void }) {
     event.stopPropagation();
-
   }
-
 
   drop(event: CdkDragDrop<TaskDto[]>, column: ColumnTask) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
 
-      this.taskService.updateTaskColumnId({ "id": event.container.data[event.currentIndex].id, "columnId": column.id }).subscribe((res) => {
-        this.ngOnInit()
-      });
+      this.taskService
+        .updateTaskColumnId({
+          id: event.container.data[event.currentIndex].id,
+          columnId: column.id,
+        })
+        .subscribe((res) => {
+          this.ngOnInit();
+        });
     }
   }
 
   dropColumn(event: CdkDragDrop<any[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
-
-    };
+    }
   }
 
+  openMenuAndFocusInput() {
+    debugger;
+    this.yourMatMenuTrigger.openMenu();
+
+  // Use a timeout to set focus after a brief delay
+  setTimeout(() => {
+    this.taskInput.nativeElement.focus();
+  }, 0);
 
 
+  }
   getColumnId(columnId: number) {
-
     this.currentColumnId = columnId;
   }
 
   openEditDialog(columnName: string, currentColumnId: number) {
-    const dialogRef = this.dialog.open(EditColumnComponent, { data: { data: columnName, currentColumnId: currentColumnId } });
-    dialogRef.afterClosed().subscribe(result => {
+    const dialogRef = this.dialog.open(EditColumnComponent, {
+      data: { data: columnName, currentColumnId: currentColumnId },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
       this.ngOnInit();
     });
   }
@@ -163,12 +195,18 @@ export class ColumnsComponent {
       if (res.isSuccessful == true) {
         this.tasks = res.data;
 
-        const dialog = this.dialog.open(TaskComponent, { autoFocus: false, data: { task: this.tasks }, height: '90%', width: '90%', panelClass: 'dialog' });
+        const dialog = this.dialog.open(TaskComponent, {
+          autoFocus: false,
+          data: { task: this.tasks },
+          height: '90%',
+          width: '90%',
+          panelClass: 'dialog',
+        });
         dialog.afterClosed().subscribe((res) => {
           this.ngOnInit();
-        })
+        });
       }
-    })
+    });
   }
 
   // GetAllProjectColumns() {
@@ -179,33 +217,32 @@ export class ColumnsComponent {
   //   })
   // }
 
-
-
   createColumn() {
-    this.columnService.CreateColumn({ 'projectId': this.projectService.getProjectLocal().id, 'name': this.columnName }).subscribe((res) => {
-      this.columnName = ''
-      this.ngOnInit()
-    })
+    this.columnService
+      .CreateColumn({
+        projectId: this.projectService.getProjectLocal().id,
+        name: this.columnName,
+      })
+      .subscribe((res) => {
+        this.columnName = '';
+        this.ngOnInit();
+      });
   }
 
   isDeleteButtonDisabled(id: number) {
     if (this.columns.length <= 1) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
-
   }
-
-
 
   CreateTask() {
     this.taskObj.columnId = this.currentColumnId;
     this.taskObj.projectId = this.projectService.getProjectLocal().id;
-    this.taskObj.assigneeId = "unassigned";
+    this.taskObj.assigneeId = 'unassigned';
     this.taskObj.reporterId = this.tokenService.tokenUserId();
-    this.taskObj.priority = 3
+    this.taskObj.priority = 3;
     //this.taskObj.endDate = this.defaultEndDate
 
     this.taskService.createTask(this.taskObj).subscribe((res) => {
@@ -213,16 +250,18 @@ export class ColumnsComponent {
         this.ngOnInit();
       }
     });
+
+    this.taskObj.name = ''; //!
   }
 
   OpenDeleteDialog(columnId: number, columnName: string) {
     if (columnId != null && columnName != null) {
       const column: ColumnDto = {
         id: columnId,
-        name: columnName
-      }
+        name: columnName,
+      };
 
-      let selectedColumn = this.columns.find(c => c.id == columnId);
+      let selectedColumn = this.columns.find((c) => c.id == columnId);
       if (selectedColumn?.tasks.length == 0) {
         // Show SweetAlert before directly deleting the column
         Swal.fire({
@@ -242,29 +281,36 @@ export class ColumnsComponent {
           }
         });
       } else {
-        const dialogRef = this.dialog.open(TransferColumnTaskComponent, { data: { column: column }, width: '540px' });
-        dialogRef.afterClosed().subscribe(result => {
-          this.columnService.TransferColumnTasks(result.transfer).subscribe((res) => {
-            if (res.isSuccessful) {
-              // Show SweetAlert before deleting the column after transferring tasks
-              Swal.fire({
-                icon: 'warning',
-                title: 'Delete Column',
-                text: 'Are you sure you want to delete this column?',
-                showCancelButton: true,
-                confirmButtonText: 'Delete',
-                cancelButtonText: 'Cancel',
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  this.columnService.DeleteColumn(column.id).subscribe((res) => {
-                    if (res.isSuccessful) {
-                      this.ngOnInit();
-                    }
-                  });
-                }
-              });
-            }
-          });
+        const dialogRef = this.dialog.open(TransferColumnTaskComponent, {
+          data: { column: column },
+          width: '540px',
+        });
+        dialogRef.afterClosed().subscribe((result) => {
+          this.columnService
+            .TransferColumnTasks(result.transfer)
+            .subscribe((res) => {
+              if (res.isSuccessful) {
+                // Show SweetAlert before deleting the column after transferring tasks
+                Swal.fire({
+                  icon: 'warning',
+                  title: 'Delete Column',
+                  text: 'Are you sure you want to delete this column?',
+                  showCancelButton: true,
+                  confirmButtonText: 'Delete',
+                  cancelButtonText: 'Cancel',
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    this.columnService
+                      .DeleteColumn(column.id)
+                      .subscribe((res) => {
+                        if (res.isSuccessful) {
+                          this.ngOnInit();
+                        }
+                      });
+                  }
+                });
+              }
+            });
         });
       }
     }
@@ -279,7 +325,9 @@ export class ColumnsComponent {
     return color;
   }
   getRandomPredefinedColor(): string {
-    const randomIndex = Math.floor(Math.random() * this.predefinedColors.length);
+    const randomIndex = Math.floor(
+      Math.random() * this.predefinedColors.length
+    );
     return this.predefinedColors[randomIndex];
   }
 
@@ -288,16 +336,15 @@ export class ColumnsComponent {
 
     const usedColors = new Set();
 
-    this.columns.forEach(column => {
+    this.columns.forEach((column) => {
       let randomColor = this.getRandomPredefinedColor();
 
       while (usedColors.has(randomColor)) {
         randomColor = this.getRandomPredefinedColor();
       }
 
-      usedColors.add(randomColor); 
+      usedColors.add(randomColor);
       this.columnColors.push(randomColor);
     });
-
   }
 }
