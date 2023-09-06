03b06d173e7b4fd7afb249e1f7786d7c34739b1e
selinkhrmn <khrmnsln159@gmail.com>
2023-09-06 22:08:02 +0300

    ..devamı

--- src/app/components/sidenav2/sidenav2.component.ts
+++ src/app/components/sidenav2/sidenav2.component.ts
@@ -71,7 +71,7 @@ export class Sidenav2Component implements OnInit{
     this.router.navigate(['create-project'])
   }
   openDialog(): void {
-    const dialogRef = this.dialog.open(CreateProjectComponent,{height: '55%',width: '45%', panelClass: 'dialog'});
+    const dialogRef = this.dialog.open(CreateProjectComponent,{height: '60%',width: '45%', panelClass: 'dialog'});
   }
 
   reload() {
