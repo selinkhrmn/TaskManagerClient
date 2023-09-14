16ca3050d1e356ff5a1614dd39a8d558c805c151
Oguzhankeserr <oguzhankeser.07@gmail.com>
2023-09-14 14:28:42 +0300

    Create-Issue-Dialog (konsol hatası ve sayfa yenileme)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Task } from 'src/app/interfaces/task';
 import { MatTable } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { ColumnsComponent } from '../board/columns/columns.component';
-import { TranslocoService} from '@ngneat/transloco';
+import { TranslocoService } from '@ngneat/transloco';
 import { FileService } from 'src/app/services/file.service';
 import { ShareComponent } from './share/share.component';
 
@@ -17,101 +17,93 @@ import { ShareComponent } from './share/share.component';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  providers:[ColumnsComponent]
+  providers: [ColumnsComponent]
 })
 export class NavbarComponent {
 
   @ViewChild(MatTable) table: MatTable<Task>;
-  taskData : any;
-  url : string;
+  taskData: any;
+  url: string;
   id = this.tokenService.getTokenId();
-  noImage= '../../assets/noImage.png';
+  noImage = '../../assets/noImage.png';
 
-    constructor(
-      public tokenService: TokenService,
-      private dialog : MatDialog,
-      private dialogRef: MatDialogRef<CreateIssueDialogComponent>,
-      public taskService : TaskService,
-      private router: Router,
-      public columnComp: ColumnsComponent,
-      public authService: AuthService,
-      public translocoService: TranslocoService,
-      private fileService: FileService
+  constructor(
+    public tokenService: TokenService,
+    private dialog: MatDialog,
+    private dialogRef: MatDialogRef<CreateIssueDialogComponent>,
+    public taskService: TaskService,
+    private router: Router,
+    public columnComp: ColumnsComponent,
+    public authService: AuthService,
+    public translocoService: TranslocoService,
+    private fileService: FileService
 
 
-    ) {}
+  ) { }
 
-    ngOnInit() {
-      this.fileService.getProfilePhoto({"userId" : this.id}).subscribe((res)=> {
-        this.url = res[res.length-1];
-      });
-    }
+  ngOnInit() {
+    this.fileService.getProfilePhoto({ "userId": this.id }).subscribe((res) => {
+      this.url = res[res.length - 1];
+    });
+  }
 
-    hasToken(): boolean {
-      return localStorage.getItem('token') !== null;
-    }
-  
-    logOut(){
-      this.authService.logOut();
-     }
+  hasToken(): boolean {
+    return localStorage.getItem('token') !== null;
+  }
 
-    routerToAdminPage() {
-      this.router.navigate(['admin-page/admin-projects']);
-    }
-    navigateToSummary() {
-      this.router.navigate(['/home/summary']); // Replace '/summary' with the actual route path of your summary component
-    }
-    
-    routerToProfileSetting() {
-      this.router.navigate(['profile-setting']);
-    }
+  logOut() {
+    this.authService.logOut();
+  }
 
-    openMailDialog() {
+  routerToAdminPage() {
+    this.router.navigate(['admin-page/admin-projects']);
+  }
+  navigateToSummary() {
+    this.router.navigate(['/home/summary']); // Replace '/summary' with the actual route path of your summary component
+  }
 
-      this.dialog.open(ShareComponent, {width: '30%', height: '60%'});
-    }
+  routerToProfileSetting() {
+    this.router.navigate(['profile-setting']);
+  }
 
-    openCreateIssueDialog() {
-      const dialog = this.dialog.open(CreateIssueDialogComponent, { data: { table: this.table }, width: '60%' });
-  
-      dialog.afterClosed().subscribe((response) => {
+  openMailDialog() {
 
-        
-  
-        if (response.isAdded) {
-            this.taskService.createTask(response.task).subscribe((res) => {
-              //window.location.reload();
-              console.log(res.data);
-              if (res.isSuccessful == true) {
-                this.taskData = res.data;
-                // this.formData = new FormData();
-                //  this.formData.append('file', response.file);
-      
-                this.fileService.saveFile(response.file, this.taskData.id.toString()).subscribe((res) => {
-                  
-                  //this.formData.append('file', e.target.files[0]);
-      
-                });
-              }
-      
-      
+    this.dialog.open(ShareComponent, { width: '30%', height: '60%' });
+  }
+
+  openCreateIssueDialog() {
+    const dialog = this.dialog.open(CreateIssueDialogComponent, { data: { table: this.table }, width: '60%' });
+
+    dialog.afterClosed().subscribe((response) => {
+
+
+
+      if (response && response.isAdded) {
+        this.taskService.createTask(response.task).subscribe((res) => {
+          //window.location.reload();
+          console.log(res.data);
+          if (res.isSuccessful == true) {
+            this.taskData = res.data;
+            // this.formData = new FormData();
+            //  this.formData.append('file', response.file);
+
+            this.fileService.saveFile(response.file, this.taskData.id.toString()).subscribe((res) => {
+
+              //this.formData.append('file', e.target.files[0]);
+
             });
-          
-        }
-        
-        
-  
-  
-       
-  
-  
-      });
-  
-  
-    }
+          }
 
-    changeLanguage(language: 'tr' | 'en') {
-      this.translocoService.setActiveLang(language);
-    }
+        });
+
+      }
+
+    });
+
+  }
+
+  changeLanguage(language: 'tr' | 'en') {
+    this.translocoService.setActiveLang(language);
+  }
 
 }
